15b520aed5aa6b2f0be8f650677d746e398cb0d9
Duy Trung <trungndph26553@gmail.com>
2023-06-08 11:11:21 +0700

    Signin: update

--- src/app/layouts/base-layout/base-layout.component.ts
+++ src/app/layouts/base-layout/base-layout.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUSser } from 'src/app/interfaces/User';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-base-layout',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./base-layout.component.scss']
 })
 export class BaseLayoutComponent {
+  user!: IUSser | null;
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
+  ngOnInit() {
+    const authenticatedUser = this.authService.isAuthenticated();
+    this.user = authenticatedUser?.user;
+    console.log(this.user);
+
+  }
+  logout() {
+    localStorage.removeItem('credential');
+    this.user = null;
+    this.router.navigate(['/signin']);
+  }
 }
